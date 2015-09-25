f729327850342d5c620fd2d68f7fb15bc58a5c9b
Ignatius Alriana Haryadi Moel <alrianatcm@gmail.com>
2015-09-25 22:17:49 +0700

    Update transmitter.cpp

--- src/transmitter.cpp
+++ src/transmitter.cpp
@@ -92,7 +92,7 @@ int main(int argc, char *argv[]) {
 			// Menbaca File yang akan dikirim
 			char *ch;
 			int i = 0;
-			fstream fin(filename.c_str(), fstream::in);
+			fstream fin(argv[e], fstream::in);
 			while (fin >> noskipws >> ch) {
 				if (sent_xonxoff == XON){
 	    				size_t len = strlen(ch);
